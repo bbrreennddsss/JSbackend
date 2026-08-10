079c934016bc266a558699311f8911d1a33674c1
brenda <87251177+bbrreennddsss@users.noreply.github.com>
2026-08-10 08:45:07 -0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log('Olá, mundo! Este é meu primeiro script Node.js!');
+
+console.log('Parabéns! Você está programando!')
+console.log('Node.js versão:', process.version);
+console.log('Sistema operacional:', process.plataform);
